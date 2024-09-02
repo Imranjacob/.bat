f3192d181faee41e697eea48351fe35b838bc5f9
Imranjacob <94866013+Imranjacob@users.noreply.github.com>
2024-09-02 10:38:54 +0800

    Update pingsrv.bat
    Multiple ping in one script

--- pingsrv.bat
+++ pingsrv.bat
@@ -17,118 +17,21 @@ echo please wait while pinging in Progress.
 time /T >>C:\Users\User\Desktop\Ping.txt
 
 
-echo pinging 10.1.5.106
-ping 10.1.5.106 -n 3 >>C:\Users\User\Desktop\Ping.txt
+echo pinging "IP"
+ping "IP" -n 3 >>C:\Users\User\Desktop\Ping.txt
 timeout /t 5 /nobreak
 
 echo pinging 10.1.5.108
-ping 10.1.5.108 -n 3 >>C:\Users\User\Desktop\Ping.txt
+ping "IP" -n 3 >>C:\Users\User\Desktop\Ping.txt
 timeout /t 5 /nobreak
 
 echo pinging 10.1.5.109
-ping 10.1.5.109 -n 3 >>C:\Users\User\Desktop\Ping.txt
+ping "IP" -n 3 >>C:\Users\User\Desktop\Ping.txt
 timeout /t 5 /nobreak
 
-echo pinging 10.1.5.4
-ping 10.1.5.4 -n 3 >>C:\Users\User\Desktop\Ping.txt
-timeout /t 5 /nobreak
-
-echo pinging 10.1.5.110
-ping 10.1.5.110 -n 3 >>C:\Users\User\Desktop\Ping.txt
-timeout /t 5 /nobreak
-
-echo pinging 10.1.5.7
-ping 10.1.5.7 -n 3 >>C:\Users\User\Desktop\Ping.txt
-timeout /t 5 /nobreak
-
-echo pinging 10.1.5.6
-ping 10.1.5.6 -n 3 >>C:\Users\User\Desktop\Ping.txt
-timeout /t 5 /nobreak
-
-echo pinging 10.1.5.15
-ping 10.1.5.15 -n 3 >>C:\Users\User\Desktop\Ping.txt
-timeout /t 5 /nobreak
-
-echo pinging 10.1.5.20
-ping 10.1.5.20 -n 3 >>C:\Users\User\Desktop\Ping.txt
-timeout /t 5 /nobreak
-
-echo pinging 10.1.5.19
-ping 10.1.5.19 -n 3 >>C:\Users\User\Desktop\Ping.txt
-timeout /t 5 /nobreak
-
-echo pinging 10.1.5.115
-ping 10.1.5.115 -n 1 >>C:\Users\User\Desktop\Ping.txt
-timeout /t 6 /nobreak
-
-echo pinging 10.1.5.17
-ping 10.1.5.17 -n 3 >>C:\Users\User\Desktop\Ping.txt
-timeout /t 5 /nobreak
-
-echo pinging 10.1.5.1
-ping 10.1.5.1 -n 3 >>C:\Users\User\Desktop\Ping.txt
-timeout /t 5 /nobreak
-
-echo pinging 10.1.5.8
-ping 10.1.5.8 -n 3 >>C:\Users\User\Desktop\Ping.txt
-timeout /t 5 /nobreak
-
-echo pinging 10.1.5.11
-ping 10.1.5.11 -n 3 >>C:\Users\User\Desktop\Ping.txt
-timeout /t 5 /nobreak
-
-echo pinging 10.1.5.150
-ping 10.1.5.150 -n 3 >>C:\Users\User\Desktop\Ping.txt
-timeout /t 5 /nobreak
-
-echo pinging 10.1.5.151
-ping 10.1.5.151 -n 1 >>C:\Users\User\Desktop\Ping.txt
-timeout /t 6 /nobreak
-
-echo pinging 192.168.10.200
-ping 192.168.10.200 -n 3 >>C:\Users\User\Desktop\Ping.txt
-timeout /t 5 /nobreak
-
-echo pinging 192.168.10.246
-ping 192.168.10.246 -n 3 >>C:\Users\User\Desktop\Ping.txt
-timeout /t 5 /nobreak
-
-echo pinging 192.168.10.100
-ping 192.168.10.100 -n 3 >>C:\Users\User\Desktop\Ping.txt
-timeout /t 5 /nobreak
-
-echo pinging 192.168.10.103
-ping 192.168.10.103 -n 3 >>C:\Users\User\Desktop\Ping.txt
-timeout /t 5 /nobreak
-
-echo pinging 192.168.10.160
-ping 192.168.10.160 -n 3 >>C:\Users\User\Desktop\Ping.txt
-timeout /t 5 /nobreak
-
-echo pinging 192.168.10.102
-ping 192.168.10.102 -n 1 >>C:\Users\User\Desktop\Ping.txt
-timeout /t 6 /nobreak
-
-echo pinging 192.168.10.167
-ping 192.168.10.167 -n 3 >>C:\Users\User\Desktop\Ping.txt
-timeout /t 5 /nobreak
-
-echo pinging 192.168.2.243
-ping 192.168.2.243 -n 3 >>C:\Users\User\Desktop\Ping.txt
-timeout /t 5 /nobreak
-
-echo pinging 192.168.2.24
-ping 192.168.2.24 -n 3 >>C:\Users\User\Desktop\Ping.txt
-timeout /t 5 /nobreak
-
-echo pinging 192.168.2.25
-ping 192.168.2.25 -n 3 >>C:\Users\User\Desktop\Ping.txt
-timeout /t 5 /nobreak
-
-echo pinging 192.168.2.27
-ping 192.168.2.27 -n 3 >>C:\Users\User\Desktop\Ping.txt
-timeout /t 5 /nobreak
-                
+echo pinging "IP"
+ping "IP" -n 3 >>C:\Users\User\Desktop\Ping.txt
+timeout /t 5 /nobreak               
 
 pause
 
